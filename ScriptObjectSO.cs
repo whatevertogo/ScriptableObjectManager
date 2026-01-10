e104ff289ef83498b091cbe00eb419d9b5d4af33
whatevertogo <1879483647@qq.com>
2026-01-11 00:57:23 +0800

    feat: 删除 QScriptObjectSO 脚本对象类

--- ScriptObjectSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[ManagedData("QScriptObjectSO")]
-[CreateAssetMenu(fileName = "New Script Object", menuName = "Scriptable Objects/Script Object")]
-public class QScriptObjectSO : ScriptableObject
-{
-    
-}
